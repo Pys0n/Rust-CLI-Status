eff970227066917bb3182d918710d736de185005
Pys0n <jason.krueger2010@web.de>
2026-05-13 20:23:03 +0200

    now uses "use cli_progress_bar"

--- examples/style_double_arrow.rs
+++ examples/style_double_arrow.rs
@@ -1,10 +1,11 @@
+use cli_progress_bar::{ProgressBar, PgBarStyle};
 use std::io::Write;
 
 fn main() {
-    let mut pg_bar = cli_progress_bar::ProgressBar::new();
+    let mut pg_bar = ProgressBar::new();
     pg_bar.set_title(String::from("Download..."));
     pg_bar.set_length(10);
-    pg_bar.set_style(cli_progress_bar::PgBarStyle::DoubleArrow);
+    pg_bar.set_style(PgBarStyle::DoubleArrow);
     pg_bar.set_show_progress(true);
     
     for i in 0..11 {
